ed66ee3b01c6f0266f904f0a0048e9beb940d821
unknown <peytond@vt.edu>
2021-12-22 10:57:24 -0500

    finished script

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 
 import openpyxl
 import os
+import shutil
 
-print("hello world\n\n\n")
 
 # set up "input.xlsx" for reading
-input_file = "C:\\Users\\Peyton.Dexter\\Highstreet Work\\Resume Copy\\For upload\\input.xlsx"
+input_file = "C:\\Users\\Peyton.Dexter\\Highstreet Work\\Resume Copy\\For upload\\input_clean.xlsx"
 wb = openpyxl.load_workbook(filename=input_file)
 ws = wb.active
 
+
+# set up source and dest
+source = "C:\\Users\\Peyton.Dexter\\Highstreet Work\\Resume Copy\\Backup_Resume_1\\"
+dest = "C:\\Users\\Peyton.Dexter\\Highstreet Work\\Resume Copy\\For upload\\Resumes\\"
+
+# for the entire spreadsheet, if name exists in source, copy to destination
+for row in ws.iter_rows(min_row=0, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
+    print(row[7].value)
+
+    if row[7].value is not None:
+        if os.path.isfile(source + row[7].value):
+            shutil.copy(source + row[7].value, dest + row[7].value)
+            print("Copied\n")
